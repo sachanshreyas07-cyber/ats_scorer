81bbef1a747fa08a7f530725f162372d1c219045
sachanshreyas07-cyber <sachanshreyas07@gmail.com>
2026-07-30 15:27:41 +0530

    Added scoring engine

--- backend/core/config.py
+++ backend/core/config.py
@@ -32,7 +32,10 @@ SUPPORTED_EXTENSIONS = {'.pdf', '.doc', '.docx'}     //Mime checking checks file
 
 SPACY_MODEL_PRIMARY="en_core_web_md" #better accuracy
 SPACY_MODEL_SECONDARY='"en_core_web_sm' 
-SENTENCE_TRANSFORMER_MODEL = os.getenv("SENTENCE_TRANSFORMER_MODEL", "all-MiniLM-L6-v2")
+SENTENCE_TRANSFORMER_MODEL = os.getenv(
+    "SENTENCE_TRANSFORMER_MODEL",
+    "ml_models/sbert_resume_matcher"
+)
 
 SCORE_WEIGHTS = {
     "formatting": 20, "keywords": 25, "content": 25,
